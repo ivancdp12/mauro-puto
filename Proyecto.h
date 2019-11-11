6d223bb1eca01fc602e4b9b3bc122c1db978e880
ivancdp12 <ivancdp12@users.noreply.github.com>
2019-11-11 13:36:31 -0600

    Inicio

---
+++ Proyecto.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int saldo();
+	float saldo=100000.0
+
+int main() {
+	int usuario;
+	int contraseña;
+
+	cout << "Bienvenido al cajero virtual de Mauro e Ivan" << end1;
+	cout << "Por favor ingrese su nombre de usuario" << end1;
+	cin >> contraseña;
+	cout << "Ingrese su pin" << end1;
+
+}
+
